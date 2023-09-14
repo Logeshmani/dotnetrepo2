5172636f526bf7f15abffd6440bbdc1cc1c02640
Kathirvel <kathirvelsivaking@gmail.com>
2023-09-14 14:25:35 +0530

    added fourth branch

--- Sample1.cs
+++ Sample1.cs
@@ -9,16 +9,18 @@
         Console.WriteLine("11992-Kavin Prakash"); //changes by 11992-kavin prakash
         Console.WriteLine("11994-keerthivasan");
         Console.WriteLine("11982-karnisha ");
+        Console.WriteLine("12000-Kathirvel Selvam")
 
         Console.Write("Enter a number : ");
         int n = Convert.ToInt32(Console.ReadLine());
-       if(n % 2==0 ){//changes done by keerthivasan
+        if (n % 2 == 0)
+        {//changes done by keerthivasan
             Console.WriteLine("The number is _even____");//changed by karnisha
         }
-        //     else
-        //     {
-        //         Console.WriteLine("The number is _____");
-        //     }
-        // }
+        else
+        {
+            Console.WriteLine("The number is _____");//changed by kathirvel
+        }
     }
+}
 }
