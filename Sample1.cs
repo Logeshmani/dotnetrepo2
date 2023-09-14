05d31156796bd8f41575522cbec0061aefd2994c
Karnisha Karunakaran <karnisha.k@itp.objectfrontier.com>
2023-09-14 14:18:24 +0530

    added third branch

--- Sample1.cs
+++ Sample1.cs
@@ -8,10 +8,12 @@
     {
         Console.WriteLine("11992-Kavin Prakash"); //changes by 11992-kavin prakash
         Console.WriteLine("11994-keerthivasan");
+        Console.WriteLine("11982-karnisha ");
+
         Console.Write("Enter a number : ");
         int n = Convert.ToInt32(Console.ReadLine());
        if(n % 2==0 ){//changes done by keerthivasan
-            Console.WriteLine("The number is _____");
+            Console.WriteLine("The number is _even____");//changed by karnisha
         }
         //     else
         //     {
